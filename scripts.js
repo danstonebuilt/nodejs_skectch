54454849e56899629adf345ad5eb08d120ca54b2
danstonebuilt <dnansel@hotmail.com>
2017-09-11 16:31:50 -0300

    cmt00
    First

---
+++ scripts.js
@@ -0,0 +1,3 @@
+$(() => {
+    alert('Its working!');
+});
---
+++ static_server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname));
+
+var products = [
+    {
+        id: 1,
+        name: 'laptop'
+    },
+    {
+        id: 2,
+        name: 'Microwave'
+    },
+    {
+        id: 3,
+        name: 'Router'
+    }
+];
+
+app.get('/', (req, res) => {
+    
+    res.send('Hello Word');
+});
+
+app.get('/homepage', (req, res) => {
+    
+    res.sendFile(__dirname+'/main.html');
+});
+
+app.listen(5000, () => {
+    
+    console.log('Server is on!');
+});
